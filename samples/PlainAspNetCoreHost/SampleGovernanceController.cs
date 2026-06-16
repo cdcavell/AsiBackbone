02c7cd4b7a93ce840123703913a5b99a16aba51a
Christopher D. Cavell <28095137+cdcavell@users.noreply.github.com>
2026-06-16 12:17:28 -0500

    Add public controller endpoint governance sample

---
+++ samples/PlainAspNetCoreHost/SampleGovernanceController.cs
@@ -0,0 +1,24 @@
+using CDCavell.AsiBackbone.AspNetCore.Endpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CDCavell.AsiBackbone.Samples.PlainAspNetCoreHost;
+
+[ApiController]
+[Route("sample/ergonomic/controller-public")]
+public sealed class SampleGovernanceController : ControllerBase
+{
+    [HttpPost]
+    [RequireGovernancePolicy(typeof(SampleControllerEndpointPolicy))]
+    [RequireLiabilityHandshake]
+    [RequireCapabilityGrant("sample.high-risk.execute")]
+    [EmitGovernanceAudit]
+    public IActionResult ExecuteHighRiskAction()
+    {
+        return Ok(new
+        {
+            message = "Public controller action executed after AsiBackbone endpoint governance metadata was evaluated."
+        });
+    }
+}
+
+public sealed class SampleControllerEndpointPolicy;

46f5867f647ddb3547494bec3ecdff45aa123ac0
Christopher D. Cavell <28095137+cdcavell@users.noreply.github.com>
2026-06-20 18:01:49 -0500

    Add options namespace for diagnostics tests - Issue #242

--- tests/CDCavell.AsiBackbone.AspNetCore.Tests/Endpoints/AsiBackboneEndpointGovernanceTests.cs
+++ tests/CDCavell.AsiBackbone.AspNetCore.Tests/Endpoints/AsiBackboneEndpointGovernanceTests.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace CDCavell.AsiBackbone.AspNetCore.Tests.Endpoints;
